c5d5de3c01efd1cf0d48a56b4c52a8f89538ba9f
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2025-01-24 10:25:53 +0900

    ページのスクロール量を表示

--- chapter4/13-demo/js/script.js
+++ chapter4/13-demo/js/script.js
@@ -4,8 +4,9 @@ const getScrollPercent = () => {
 
   const pageHeight = document.documentElement.scrollHeight;
   const viewHeight = document.documentElement.clientHeight;
+  const percentage = scrolled / (pageHeight - viewHeight) * 100;
 
-  // console.log(`ページの高さ：${pageHeight}、表示領域の高さ：${viewHeight}`)
+  document.querySelector('#bar').style.width = `${percentage}%`;
 };
 
 window.addEventListener('scroll', getScrollPercent);
